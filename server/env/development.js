c22cd15afd9bc82968bd70642039f06c055b9a39
Jason Unger <jasonscottunger@gmail.com>
2016-10-15 17:31:58 -0400

    Added seed functionality

--- server/env/development.js
+++ server/env/development.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   DATABASE_URI: 'postgres://localhost:5432/acagig',
   SESSION_SECRET: process.env.SESSION_SECRET,
-  LOGGING: console.log
+  LOGGING: false
 };
