7e50569e1dcafd3a2863a37881675de4dd9caabc
Jason Unger <jasonscottunger@gmail.com>
2016-10-15 01:36:33 -0400

    Adding postgres to my Travis CI yaml

--- server/env/testing.js
+++ server/env/testing.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DATABASE_URI: 'postgres://localhost:5432/acagig-test',
+  DATABASE_URI: 'postgres://localhost:5432/acagig_test',
   SESSION_SECRET: 'Batman is my real dad',
   LOGGING: false
 };
